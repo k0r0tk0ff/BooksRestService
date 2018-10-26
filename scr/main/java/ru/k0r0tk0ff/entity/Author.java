31ecfe8d7f86f251c1964667abf95eb282fa56be
Alexandr <korotkov34reg@gmail.com>
2018-10-26 13:08:37 +0300

    Create Author.java

---
+++ scr/main/java/ru/k0r0tk0ff/entity/Author.java
@@ -0,0 +1,51 @@
+package ru.k0r0tk0ff.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by korotkov_a_a on 26.10.2018.
+ */
+
+@Entity
+public class Author {
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Author(String name, Set<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "author_id")
+    private Long authorId;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Book> books = new HashSet<Book>();
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
