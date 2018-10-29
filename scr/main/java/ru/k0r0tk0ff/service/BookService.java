d5df431ffb851f014a09dc522980b7ee790f90b1
Alexandr <korotkov34reg@gmail.com>
2018-10-29 16:52:35 +0300

    Create BookService.java

---
+++ scr/main/java/ru/k0r0tk0ff/service/BookService.java
@@ -0,0 +1,31 @@
+package ru.k0r0tk0ff.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.k0r0tk0ff.entity.Book;
+import ru.k0r0tk0ff.repository.BookRepo;
+
+import java.util.List;
+
+/**
+ * Created by korotkov_a_a on 29.10.2018.
+ */
+
+@Service
+public class BookService {
+
+    private BookRepo bookRepo;
+
+    @Autowired
+    public BookService(BookRepo bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepo.getOne(id);
+    }
+
+    public List<Book> getAllBooks() {
+        return this.bookRepo.findAll();
+    }
+}
