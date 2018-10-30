38764ab873495e295f1f0dee635e34ffb27fc33e
Alexandr <korotkov34reg@gmail.com>
2018-10-30 09:06:47 +0300

    Update BookService.java

--- scr/main/java/ru/k0r0tk0ff/service/BookService.java
+++ scr/main/java/ru/k0r0tk0ff/service/BookService.java
@@ -1,11 +1,11 @@
 package ru.k0r0tk0ff.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import ru.k0r0tk0ff.entity.Book;
-import ru.k0r0tk0ff.repository.BookRepo;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
+        import ru.k0r0tk0ff.entity.Book;
+        import ru.k0r0tk0ff.repository.BookRepo;
 
-import java.util.List;
+        import java.util.List;
 
 /**
  * Created by korotkov_a_a on 29.10.2018.
