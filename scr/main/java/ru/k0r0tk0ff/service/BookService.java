9a5f950a03130560e68adc2802df08a748b2625b
Alexandr <korotkov34reg@gmail.com>
2018-10-31 13:54:49 +0300

    Update BookService.java

--- scr/main/java/ru/k0r0tk0ff/service/BookService.java
+++ scr/main/java/ru/k0r0tk0ff/service/BookService.java
@@ -3,11 +3,9 @@ package ru.k0r0tk0ff.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
-import ru.k0r0tk0ff.entity.Book;
-import ru.k0r0tk0ff.repository.BookRepo;
-import java.util.HashMap;
+import ru.k0r0tk0ff.entity.Author;
+import ru.k0r0tk0ff.repository.AuthorRepo;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -15,111 +13,56 @@ import java.util.Optional;
  */
 
 @Service
-public class BookService {
-    private static final Map<String, String> SUCCESS_UPDATED = new HashMap<String, String>() {
-        { put("SUCCESS", "The book was successfully updated.");}
-    };
-    private static final Map<String, String> SUCCESS_BOOK_WAS_DELETED = new HashMap<String, String>() {
-        { put("SUCCESS", "The book was successfully deleted.");}
-    };
+public class AuthorService {
 
-    private static final Map<String, String> SUCCESS_SAVED = new HashMap<String, String>() {
-        { put("SUCCESS", "The book was successfully saved.");}
-    };
-    private static final Map<String, String> FAIL_AUTHOR_NOT_EXIST = new HashMap<String, String>() {
-        { put("FAIL", "The author does not exist! Check parameter \"author_id\"");}
-    };
-    private static final Map<String, String> FAIL_BOOK_EXIST = new HashMap<String, String>() {
-        { put("FAIL", "The book exist!");}
-    };
-    private static final Map<String, String> FAIL_BOOK_NOT_EXIST = new HashMap<String, String>() {
-        { put("FAIL", "The book not exist!");}
-    };
-
-    private BookRepo bookRepo;
-    private AuthorService authorService;
+    private AuthorRepo authorRepo;
 
     @Autowired
-    public BookService(BookRepo bookRepo, AuthorService authorService) {
-        this.bookRepo = bookRepo;
-        this.authorService = authorService;
+    public AuthorService(AuthorRepo authorRepo) {
+        this.authorRepo = authorRepo;
     }
 
-    public Optional<Book> getBookById(Long id) {
-        return bookRepo.findById(id);
+    public Optional<Author> getAuthorById(Long id) {
+        return authorRepo.findById(id);
     }
 
-    public List<Book> getAllBooks() {
-        return this.bookRepo.findAll();
+    public List<Author> getAllAuthors() {
+        return authorRepo.findAll();
     }
 
-    private boolean isBookExist(Book book) {
-        Example<Book> example = Example.of(book);
-        return this.bookRepo.findOne(example).isPresent();
+    public void saveAuthorToAuthorRepo(Author author) {
+        this.authorRepo.save(author);
     }
 
-    private boolean isBookExist(Long id) {
-        return this.bookRepo.existsById(id);
+    public void createAuthorByName(String name) {
+        Author author = new Author();
+        author.setName(name);
+        saveAuthorToAuthorRepo(author);
     }
 
-    private void saveBookInBookRepo(Book book) {
-        this.bookRepo.save(book);
+    public boolean isAuthorExist(Author author) {
+        Example<Author> example = Example.of(author);
+        return this.authorRepo.exists(example);
     }
 
-    public Map<String, String> createBook(Map<String, String> bookParameters) {
-        Book book = bookBuilder(bookParameters);
-        if(isBookExist(book)) {
-            return FAIL_BOOK_EXIST;
-        }
-        if(!authorService.isAuthorExist(bookParameters.get("author_name"))) {
-            authorService.createAuthorByName(bookParameters.get("author_name"));
-        }
-        book.setBookId((long) (bookRepo.findAll().lastIndexOf(book)));
-        saveBookInBookRepo(book);
-        return SUCCESS_SAVED;
+    public boolean isAuthorExist(Long authorId) {
+        return authorRepo.existsById(authorId);
     }
 
-    public Map<String,String> updateBook(Map<String, String> bookParameters) {
-        if(!authorService.isAuthorExist(bookParameters.get("author_name"))){
-            return FAIL_AUTHOR_NOT_EXIST;
-        }
-        if(!isBookExist(Long.parseLong(bookParameters.get("book_id")))) {
-            return FAIL_BOOK_NOT_EXIST;
-        }
-        Book book = bookBuilder(bookParameters);
-        book.setBookId(Long.parseLong(bookParameters.get("book_id")));
-        this.bookRepo.save(book);
-        return SUCCESS_UPDATED;
+    public boolean isAuthorExist(String authorName) {
+        Author author = new Author(authorName);
+        return isAuthorExist(author);
     }
 
-    private Book bookBuilder(Map<String, String> bookParameters) {
-        String name = bookParameters.get("name");
-        String price = bookParameters.get("price");
-        String authorName = bookParameters.get("author_name");
-        Book book = new Book();
-        book.setAuthor(authorService.getOrCreateAndGetAuthorByName(authorName));
-        book.setName(name);
-        book.setPrice(Double.parseDouble(price));
-        return book;
-    }
-
-    public Map<String, String> delBookById(Long id) {
-        if(getBookById(id).isPresent()) {
-            Book book = getBookById(id).get();
-            bookRepo.delete(book);
+    public Author getOrCreateAndGetAuthorByName(String authorName) {
+        Author author = new Author(authorName);
+        Example<Author> example = Example.of(author);
+        Optional<Author> savedauthor = authorRepo.findOne(example);
+        if (savedauthor.isPresent()) {
+            author = savedauthor.get();
         } else {
-            return FAIL_BOOK_NOT_EXIST;
+            saveAuthorToAuthorRepo(author);
         }
-        return SUCCESS_BOOK_WAS_DELETED;
-    }
-
-    public Map<String,String> getBookParameters(Long id) {
-        Map<String, String> bookParameters = new HashMap<>(4);
-        Book book = this.bookRepo.getOne(id);
-        bookParameters.put("book_id", String.valueOf(id));
-        bookParameters.put("name", book.getName());
-        bookParameters.put("price", String.valueOf(book.getPrice()));
-        bookParameters.put("author_name", book.getAuthor().getName());
-        return bookParameters;
+        return author;
     }
 }
