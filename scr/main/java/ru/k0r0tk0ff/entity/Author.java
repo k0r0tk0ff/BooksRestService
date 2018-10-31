74eca6489e61171d4b7b4cbb3d0c2946ea6ddbe7
Alexandr <korotkov34reg@gmail.com>
2018-10-31 10:05:57 +0300

    Update Author.java

--- scr/main/java/ru/k0r0tk0ff/entity/Author.java
+++ scr/main/java/ru/k0r0tk0ff/entity/Author.java
@@ -60,4 +60,17 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
