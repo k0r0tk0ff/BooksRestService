321f4a09c6805f900f661ceb2077c2073970473f
Alexandr <korotkov34reg@gmail.com>
2018-11-01 16:20:09 +0300

    Update data-h2.sql

--- scr/main/java/resources/data-h2.sql
+++ scr/main/java/resources/data-h2.sql
@@ -1,14 +1,17 @@
-INSERT INTO AUTHOR (NAME) VALUES ('Пушкин');
-INSERT INTO AUTHOR (NAME) VALUES ('Толстой');
-INSERT INTO AUTHOR (NAME) VALUES ('Шолохов');
-CREATE CONSTANT pushkin_id VALUE (SELECT AUTHOR_ID FROM author WHERE NAME = 'Пушкин');
-CREATE CONSTANT sholohov_id VALUE (SELECT AUTHOR_ID FROM author WHERE NAME = 'Шолохов');
-CREATE CONSTANT tolstoy_id VALUE (SELECT AUTHOR_ID FROM author WHERE NAME = 'Толстой');
-INSERT INTO BOOK (AUTHOR_ID, NAME, PRICE) VALUES (pushkin_id, 'Пиковая дама', '400.50');
-INSERT INTO BOOK (AUTHOR_ID, NAME, PRICE) VALUES (pushkin_id, 'Капитанская дочка', '420.90');
-INSERT INTO BOOK (AUTHOR_ID, NAME, PRICE) VALUES (pushkin_id, 'Медный всадник', '1410.10');
-INSERT INTO BOOK (AUTHOR_ID, NAME, PRICE) VALUES (sholohov_id, 'Тихий Дон', '800.00');
-INSERT INTO BOOK (AUTHOR_ID, NAME, PRICE) VALUES (tolstoy_id, 'Война и мир', '1500.00');
+INSERT INTO AUTHOR (name) VALUES ('Пушкин');
+INSERT INTO AUTHOR (name) VALUES ('Толстой');
+INSERT INTO AUTHOR (name) VALUES ('Шолохов');
+CREATE CONSTANT pushkin_id VALUE (SELECT author_id FROM author WHERE NAME = 'Пушкин');
+CREATE CONSTANT sholohov_id VALUE (SELECT author_id FROM author WHERE NAME = 'Шолохов');
+CREATE CONSTANT tolstoy_id VALUE (SELECT author_id FROM author WHERE NAME = 'Толстой');
+INSERT INTO BOOK (author_id, name, price) VALUES (pushkin_id, 'Пиковая дама', '400.50');
+INSERT INTO BOOK (author_id, name, price) VALUES (pushkin_id, 'Капитанская дочка', '420.90');
+INSERT INTO BOOK (author_id, name, price) VALUES (pushkin_id, 'Медный всадник', '1410.10');
+INSERT INTO BOOK (author_id, name, price) VALUES (sholohov_id, 'Тихий Дон', '800.00');
+INSERT INTO BOOK (author_id, name, price) VALUES (tolstoy_id, 'Война и мир', '1500.00');
+INSERT INTO WISHLIST (wishlist_id, book_id, count) VALUES ('1', '1', '5');
+INSERT INTO WISHLIST (wishlist_id, book_id, count) VALUES ('2', '2', '4');
 DROP constant pushkin_id;
 DROP constant sholohov_id;
 DROP constant tolstoy_id;
+
