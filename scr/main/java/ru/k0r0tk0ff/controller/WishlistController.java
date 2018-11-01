5951d185df87daca7f6d122ccc3f2f689448191e
Alexandr <korotkov34reg@gmail.com>
2018-11-01 16:24:45 +0300

    Update WishlistController.java

--- scr/main/java/ru/k0r0tk0ff/controller/WishlistController.java
+++ scr/main/java/ru/k0r0tk0ff/controller/WishlistController.java
@@ -5,25 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import ru.k0r0tk0ff.entity.Wishlist;
+import org.springframework.web.bind.annotation.*;
 import ru.k0r0tk0ff.service.WishlistService;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 /**
  * Created by korotkov_a_a on 29.10.2018.
  */
 
 @RestController
-public class WishlistController {
+public class WishlistController implements EntityController{
 
     private static final Logger logger = LoggerFactory.getLogger(WishlistController.class);
-
     private WishlistService wishlistService;
 
     @Autowired
@@ -31,21 +26,61 @@ public class WishlistController {
         this.wishlistService = wishlistService;
     }
 
-    @RequestMapping(value = "/api/wishlists", method = RequestMethod.GET)
-    public ResponseEntity<List<Wishlist>> listAllWishlists() {
-        List<Wishlist> wishlists = wishlistService.getAllWishlists();
-        if (wishlists.isEmpty()) {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
+    @GetMapping(value = "/api/wishlist/{id}")
+    public ResponseEntity<?> getEntityById(@PathVariable("id") Long id) {
+        logger.info("Get wishlist with id = {}", id);
+        if(!wishlistService.doesWishlistExist(id)){
+            logger.error("Wishlist with id = {} not found!", id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        return new ResponseEntity<List<Wishlist>>(wishlists, HttpStatus.OK);
+        Map<String,String> wishlistExtendData = wishlistService.getWishlistData(id);
+        return new ResponseEntity<>(wishlistExtendData, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/api/wishlist/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Wishlist> getWishlistById(@PathVariable("id") Long id){
-        logger.info("Get wishlist with id = {}", id);
-        Optional<Wishlist> wishlist = wishlistService.getWishlistById(id);
-        if(wishlist.isPresent()) {return new ResponseEntity<Wishlist>(wishlist.get(), HttpStatus.OK);}
-        logger.error("Wishlist with id = {} not found!", id);
-        return new ResponseEntity<Wishlist>(HttpStatus.NO_CONTENT);
+    @DeleteMapping(value = "/api/wishlist/{id}")
+    public ResponseEntity<?> delEntityById(@PathVariable("id") Long id){
+        if(!wishlistService.doesWishlistExist(id)) {
+            logger.error("Wishlist with id = \"{}\" does not exist!", id);
+            return new ResponseEntity<>("Wishlist does not exist!", HttpStatus.NOT_FOUND);
+        }
+        wishlistService.deleteWishlistById(id);
+        logger.info("Wishlist with id = \"{}\" was successfully delete.", id);
+        return new ResponseEntity<>("Delete wishlist success.", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/wishlists")
+    public ResponseEntity<?> getAllEntities() {
+        List<Map<String,String>> wishlists = wishlistService.getAllWishlists();
+        if (wishlists.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(wishlists, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/api/wishlist")
+    public ResponseEntity<?> addEntity(@RequestBody Map<String, String> wishlistParameters) {
+
+        if(wishlistService.isInputParameterBookIdValid(wishlistParameters)) {
+            String bookIdValue = wishlistParameters.get("bookId");
+            if (wishlistService.doesBookExistByBookId(bookIdValue)) {
+                wishlistService.incrementCountInWishlist(bookIdValue);
+                logger.info("Increment count for book with bookId = \"{}\" success.", bookIdValue);
+                return new ResponseEntity<>("Add book to wishlist success.", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Book not found.", HttpStatus.NOT_FOUND);
+        } else return new ResponseEntity<>("Incorrect input.", HttpStatus.CONFLICT);
+    }
+
+    @PutMapping(value = "/api/wishlist")
+    public ResponseEntity<?> updateEntity(@RequestBody Map<String, String> wishlistParameters) {
+        if(wishlistService.isInputParametersValid(wishlistParameters)) {
+            String bookIdValue = wishlistParameters.get("bookId");
+            if (wishlistService.doesBookExistByBookId(bookIdValue)) {
+                wishlistService.updateWishlist(wishlistParameters);
+                logger.info("Update count for book with bookId = \"{}\" success.", bookIdValue);
+                return new ResponseEntity<>("Update book success.", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Book not found.", HttpStatus.NOT_FOUND);
+        } else return new ResponseEntity<>("Incorrect input.", HttpStatus.CONFLICT);
     }
 }
