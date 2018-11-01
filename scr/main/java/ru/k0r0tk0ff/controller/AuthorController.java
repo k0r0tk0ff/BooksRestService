d70114f0cb504c6ef979ee5db4afba4d7c6973a8
Alexandr <korotkov34reg@gmail.com>
2018-11-01 16:25:29 +0300

    Update AuthorController.java

--- scr/main/java/ru/k0r0tk0ff/controller/AuthorController.java
+++ scr/main/java/ru/k0r0tk0ff/controller/AuthorController.java
@@ -10,6 +10,7 @@ import ru.k0r0tk0ff.entity.Author;
 import ru.k0r0tk0ff.service.AuthorService;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -17,7 +18,8 @@ import java.util.Optional;
  */
 
 @RestController
-public class AuthorController {
+public class AuthorController implements EntityController {
+//public class AuthorController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthorController.class);
 
@@ -28,8 +30,27 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
+    @GetMapping(value = "/api/author/{id}")
+    public ResponseEntity<Author> getEntityById(@PathVariable("id") Long id){
+        logger.info("Get author with id = {}", id);
+        Optional<Author> author = authorService.getAuthorById(id);
+        if(author.isPresent()) {return new ResponseEntity<Author>(author.get(), HttpStatus.OK);}
+        logger.error("Author with id {} not found!", id);
+        return new ResponseEntity<Author>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping(value = "/api/author/{id}")
+    public ResponseEntity<String> delEntityById(@PathVariable("id") Long id) {
+        if (!authorService.isAuthorExist(id)) {
+            logger.error("Author does not exist!");
+            return new ResponseEntity<>("Author does not exist!", HttpStatus.CONFLICT);
+        }
+        authorService.deleteAuthorById(id);
+        return new ResponseEntity<String>("Delete author success.", HttpStatus.OK);
+    }
+
     @GetMapping(value = "/api/authors")
-    public ResponseEntity<List<Author>> listAllAuthors() {
+    public ResponseEntity<List<Author>> getAllEntities() {
         List<Author> authors = authorService.getAllAuthors();
         if (authors.isEmpty()) {
             return new ResponseEntity(HttpStatus.NO_CONTENT);
@@ -37,21 +58,29 @@ public class AuthorController {
         return new ResponseEntity<>(authors, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/api/author/{id}")
-    public ResponseEntity<Author> getAuthorById(@PathVariable("id") Long id){
-        logger.info("Get author with id = {}", id);
-        Optional<Author> author = authorService.getAuthorById(id);
-        if(author.isPresent()) {return new ResponseEntity<>(author.get(), HttpStatus.OK);}
-        logger.error("Author with id {} not found!", id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @PostMapping(value = "/api/author")
+    public ResponseEntity<?> addEntity(@RequestBody Map<String, String> authorParameters) {
+        if(authorService.isInputParameterNameValid(authorParameters)) {
+        Author author = new Author(authorParameters.get("name"));
+        if (authorService.isAuthorExist(author)) {
+            logger.error("Author \"{}\" exist! ", author.getName());
+            return new ResponseEntity<>("Author exist.", HttpStatus.CONFLICT);
+        }
+        authorService.saveAuthor(author);
+        return new ResponseEntity<>("Create author success.", HttpStatus.OK);
+        } else return new ResponseEntity<>("Incorrect input.", HttpStatus.CONFLICT);
     }
 
-    @PostMapping(value = "/api/author")
-    public ResponseEntity<Author> setAuthor(@PathVariable("id") Long id){
-        logger.info("Create author with id = {}", id);
-        Optional<Author> author = authorService.getAuthorById(id);
-        if(author.isPresent()) {return new ResponseEntity<>(author.get(), HttpStatus.OK);}
-        logger.error("Author with id {} not found!", id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @PutMapping(value = "/api/author")
+    public ResponseEntity<?> updateEntity(@RequestBody Map<String, String> authorParameters) {
+        if (authorService.isInputParametersForUpdateValid(authorParameters)) {
+        if (!authorService.isAuthorExist(Long.parseLong(authorParameters.get("authorId")))) {
+            logger.error("Author does not exist!");
+            return new ResponseEntity<>("Author does not exist!", HttpStatus.CONFLICT);
+        }
+        authorService.updateAuthor(authorParameters);
+        return new ResponseEntity<>("Update author success.", HttpStatus.OK);
+        } else return new ResponseEntity<>("Incorrect input.", HttpStatus.CONFLICT);
     }
 }
+
